fa1597ee452b33ee538d84480a867fae664aebf3
ColinDShepard <ColinDShepard@gmail.com>
2020-10-18 13:22:30 -0700

    Setup javaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+        StackPane mainLayout = new StackPane();
+
+
+        Button button = new Button("Click me");
+        mainLayout.getChildren().add(button);
+
+        Scene scene = new Scene(mainLayout, 600, 600);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
